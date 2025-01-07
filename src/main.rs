7b251a623ccf22fab5a8c95929d9b1958680f034
Haoxuan Guo <haoxuan_guo@foxmail.com>
2025-01-07 22:18:56 +0800

    重构主函数以使用get_fermi_nai_filenames函数获取文件名，移除硬编码的文件名数组，并优化结果处理逻辑

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,6 @@ fn get_fermi_nai_filenames(epoch: Epoch) -> Vec<String> {
                 h,
             )
         })
-        .inspect(|x| println!("{}", x))
         .map(|x| Regex::new(&x).unwrap())
         .map(|re| {
             std::fs::read_dir(&folder)
@@ -51,36 +50,23 @@ fn get_fermi_nai_filenames(epoch: Epoch) -> Vec<String> {
                 })
                 .unwrap()
         })
+        .map(|x| format!("{}/{}", folder, x))
         .collect::<Vec<_>>()
 }
 fn main() {
-    // let filenames = [
-    //     "current/glg_tte_n0_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_n1_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_n2_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_n3_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_n4_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_n5_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_n6_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_n7_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_n8_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_n9_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_na_230101_00z_v00.fit.gz",
-    //     "current/glg_tte_nb_230101_00z_v00.fit.gz",
-    // ];
-    // let results = calculate_fermi_nai(&filenames);
-    // let df: DataFrame = df!(
-    //     "start" => results.iter().map(|x| x.start.to_string()).collect::<Vec<_>>(),
-    //     "stop" => results.iter().map(|x| x.stop.to_string()).collect::<Vec<_>>(),
-    //     "bin_size_min" => results.iter().map(|x| (x.bin_size_min.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
-    //     "bin_size_max" => results.iter().map(|x| (x.bin_size_max.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
-    //     "bin_size_best" => results.iter().map(|x| (x.bin_size_best.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
-    //     "delay" => results.iter().map(|x| (x.delay.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
-    //     "count" => results.iter().map(|x| x.count).collect::<Vec<_>>(),
-    //     "average" => results.iter().map(|x| x.average).collect::<Vec<_>>(),
-    // )
-    // .unwrap();
-    // println!("{}", df);
     let filenames = get_fermi_nai_filenames(Epoch::from_str("2023-01-01T00:00:00").unwrap());
-    println!("{:?}", filenames);
+    let filenames_str: Vec<&str> = filenames.iter().map(|s| s.as_str()).collect();
+    let results = calculate_fermi_nai(&filenames_str);
+    let df: DataFrame = df!(
+        "start" => results.iter().map(|x| x.start.to_string()).collect::<Vec<_>>(),
+        "stop" => results.iter().map(|x| x.stop.to_string()).collect::<Vec<_>>(),
+        "bin_size_min" => results.iter().map(|x| (x.bin_size_min.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
+        "bin_size_max" => results.iter().map(|x| (x.bin_size_max.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
+        "bin_size_best" => results.iter().map(|x| (x.bin_size_best.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
+        "delay" => results.iter().map(|x| (x.delay.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
+        "count" => results.iter().map(|x| x.count).collect::<Vec<_>>(),
+        "average" => results.iter().map(|x| x.average).collect::<Vec<_>>(),
+    )
+    .unwrap();
+    println!("{}", df);
 }
