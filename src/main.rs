f53bfc373ce49246acc096da7ba169b02045f63c
Haoxuan Guo <haoxuan_guo@foxmail.com>
2025-01-07 22:28:20 +0800

    重构主函数以使用时间序列生成文件名，移除旧的文件名处理逻辑，优化代码结构

--- src/main.rs
+++ src/main.rs
@@ -54,19 +54,26 @@ fn get_fermi_nai_filenames(epoch: Epoch) -> Vec<String> {
         .collect::<Vec<_>>()
 }
 fn main() {
-    let filenames = get_fermi_nai_filenames(Epoch::from_str("2023-01-01T00:00:00").unwrap());
-    let filenames_str: Vec<&str> = filenames.iter().map(|s| s.as_str()).collect();
-    let results = calculate_fermi_nai(&filenames_str);
-    let df: DataFrame = df!(
-        "start" => results.iter().map(|x| x.start.to_string()).collect::<Vec<_>>(),
-        "stop" => results.iter().map(|x| x.stop.to_string()).collect::<Vec<_>>(),
-        "bin_size_min" => results.iter().map(|x| (x.bin_size_min.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
-        "bin_size_max" => results.iter().map(|x| (x.bin_size_max.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
-        "bin_size_best" => results.iter().map(|x| (x.bin_size_best.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
-        "delay" => results.iter().map(|x| (x.delay.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
-        "count" => results.iter().map(|x| x.count).collect::<Vec<_>>(),
-        "average" => results.iter().map(|x| x.average).collect::<Vec<_>>(),
-    )
-    .unwrap();
-    println!("{}", df);
+    let start = Epoch::from_str("2023-01-01T00:00:00").unwrap();
+    let end = Epoch::from_str("2024-01-01T00:00:00").unwrap();
+    let step = 1.hours();
+    let time_series = TimeSeries::inclusive(start, end, step);
+    for epoch in time_series {
+        let filenames = get_fermi_nai_filenames(epoch);
+        println!("{:#?}", filenames);
+    }
+    // let filenames_str: Vec<&str> = filenames.iter().map(|s| s.as_str()).collect();
+    // let results = calculate_fermi_nai(&filenames_str);
+    // let df: DataFrame = df!(
+    //     "start" => results.iter().map(|x| x.start.to_string()).collect::<Vec<_>>(),
+    //     "stop" => results.iter().map(|x| x.stop.to_string()).collect::<Vec<_>>(),
+    //     "bin_size_min" => results.iter().map(|x| (x.bin_size_min.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
+    //     "bin_size_max" => results.iter().map(|x| (x.bin_size_max.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
+    //     "bin_size_best" => results.iter().map(|x| (x.bin_size_best.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
+    //     "delay" => results.iter().map(|x| (x.delay.total_nanoseconds() / 1000) as u32).collect::<Vec<_>>(),
+    //     "count" => results.iter().map(|x| x.count).collect::<Vec<_>>(),
+    //     "average" => results.iter().map(|x| x.average).collect::<Vec<_>>(),
+    // )
+    // .unwrap();
+    // println!("{}", df);
 }
