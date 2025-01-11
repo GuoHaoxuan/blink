29616b979f642f8bba5406311b630a4fc201230b
Haoxuan Guo <haoxuan_guo@foxmail.com>
2025-01-11 19:20:56 +0800

    更新重置卡住任务的SQL脚本，移除更新时间条件，确保状态为“运行中”的任务可以被重置

--- reset_stuck.sql
+++ reset_stuck.sql
@@ -4,5 +4,4 @@ SET
     updated_at = DATETIME ('now'),
     retry_times = retry_times + 1
 WHERE
-    updated_at < DATETIME ('now', '-12 hours')
-    AND status = 'Running';
+    status = 'Running';
