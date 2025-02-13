92523d69ab00db73d7243ee51752c3368adc9037
Haoxuan Guo <haoxuan_guo@foxmail.com>
2025-02-13 20:36:17 +0800

    重构main函数，添加local_test函数以测试calculate_fermi_nai；优化Fermi计算的调用方式

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod search;
 
 use database::{fail_task, finish_task, get_task};
 use rusqlite::Connection;
-use search::fermi::process;
+use search::fermi::{calculate_fermi_nai, process};
 
 fn consume() {
     let hostname = hostname::get().unwrap().into_string().unwrap();
@@ -28,6 +28,36 @@ fn consume() {
     }
 }
 
+fn local_test() {
+    let filenames = [
+        "2023-01-01/glg_tte_n0_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_n1_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_n2_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_n3_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_n4_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_n5_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_n6_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_n7_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_n8_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_n9_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_na_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_nb_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_b0_230101_00z_v00.fit.gz",
+        "2023-01-01/glg_tte_b1_230101_00z_v00.fit.gz",
+    ];
+    let results = calculate_fermi_nai(&filenames);
+    match results {
+        Ok(results) => {
+            for result in results {
+                println!("{:?}", result);
+            }
+        }
+        Err(e) => {
+            println!("{:?}", e);
+        }
+    }
+}
+
 fn main() {
-    consume();
+    local_test();
 }
