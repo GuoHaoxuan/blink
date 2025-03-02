19c411434a97110ac7f301173c33e4dbfa545a97
Haoxuan Guo <haoxuan_guo@foxmail.com>
2025-03-02 20:02:13 +0800

    更新绘图逻辑，使用线条表示事件能量范围，调整图例和坐标轴标签以提高可读性

--- plot.py
+++ plot.py
@@ -1,37 +1,56 @@
 import matplotlib.pyplot as plt
 import json
 from dateutil import parser
+import matplotlib.patches as mpatches
 
 
 def main():
-    with open("result_9.json", "r") as file:
+    with open("result_0.json", "r") as file:
         data = json.load(file)
 
-    # example 2023-01-01T00:01:49.647608064 UTC
     start = parser.isoparse(data["start"][:-4])
     stop = parser.isoparse(data["stop"][:-4])
-    print(f"Start: {start}")
-    print(f"Stop: {stop}")
 
     # 解析事件时间戳并绘制图表
     event_times = [
         (parser.isoparse(event["time"][:-4]) - start).total_seconds() * 1e3
         for event in data["events"]
     ]
-    pha_values = [event["pha"] for event in data["events"]]
 
-    plt.scatter(event_times, pha_values)
+    plt.figure(dpi=600)
+    for time, event in zip(event_times, data["events"]):
+        plt.plot(
+            [time, time],
+            [event["energy"]["start"], event["energy"]["stop"]],
+            color="C0" if event["detector"][:3] == "NaI" else "C1",
+        )
+
     plt.axvline(x=0, color="red", linestyle="--")
     plt.axvline(x=(stop - start).total_seconds() * 1e3, color="red", linestyle="--")
+    plt.yscale("log")
+    plt.ylabel("Energy (keV)")
+    plt.xlabel("Time (ms)")
+
     plt.twinx()
-    plt.hist(event_times, bins=50, alpha=0.3, color="gray", label="Event Histogram")
+    plt.hist(
+        event_times,
+        bins=50,
+        color="C2",
+        histtype="step",
+        alpha=0.3,
+    )
     plt.xlim(-1, (stop - start).total_seconds() * 1e3 + 1)
     plt.ylabel("Event Count")
-    plt.legend(loc="upper right")
-    plt.xlabel("Time (ms)")
-    plt.ylabel("PHA")
-    plt.title("PHA vs Time from result_0.json")
-    plt.xticks(rotation=45)
+    handles = [
+        plt.Line2D([0], [0], color="C0", label="NaI"),
+        plt.Line2D([0], [0], color="C1", label="BGO"),
+        mpatches.Patch(edgecolor="C2", label="Light Curve", fill=False),
+    ]
+
+    labels = [handle.get_label() for handle in handles]
+
+    plt.legend(handles, labels)
+
     plt.tight_layout()
     plt.savefig("pha_vs_time.png")
 
