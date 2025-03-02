4962ff53417bbbe2f2de4b68c371c5310a744d66
Haoxuan Guo <haoxuan_guo@foxmail.com>
2025-03-02 18:58:08 +0800

    添加serde依赖并更新Interval结构体以支持序列化

--- src/types/interval.rs
+++ src/types/interval.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use serde::Serialize;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Debug, Serialize)]
 pub(crate) struct Interval<T> {
     pub(crate) start: T,
     pub(crate) stop: T,
