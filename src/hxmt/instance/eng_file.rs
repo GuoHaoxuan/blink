6a2ff380f21900bd2ad884d760785446777b8653
Haoxuan Guo <haoxuan_guo@foxmail.com>
2025-03-25 14:23:21 +0800

    添加eng_file和sci_file模块，分别实现HE_Eng和HE_Evt_Src数据的读取和处理

---
+++ src/hxmt/instance/eng_file.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+
+pub(crate) struct EngFile {
+    // HDU 1: HE_Eng
+    // Only extract useful columns
+    time: Vec<i32>,
+    bus_time_bdc: Vec<[u8; 6]>,
+}
+
+impl EngFile {
+    pub(crate) fn new(filename: &str) -> Result<Self> {
+        let mut fptr = fitsio::FitsFile::open(filename)?;
+
+        // HDU 1: HE_Eng
+        let eng = fptr.hdu("HE_Eng")?;
+        let time = eng.read_col::<i32>(&mut fptr, "Time")?;
+        let bus_time_bdc_raw: Vec<u8> = eng.read_col(&mut fptr, "Bus_Time_BDC")?;
+        let mut bus_time_bdc = Vec::with_capacity(bus_time_bdc_raw.len() / 6);
+        for chunk in bus_time_bdc_raw.chunks_exact(6) {
+            let mut array = [0; 6];
+            array.copy_from_slice(chunk);
+            bus_time_bdc.push(array);
+        }
+        Ok(Self { time, bus_time_bdc })
+    }
+}
--- src/hxmt/instance/mod.rs
+++ src/hxmt/instance/mod.rs
@@ -1,4 +1,6 @@
+mod eng_file;
 mod event_file;
 mod instance_impl;
+mod sci_file;
 
 pub(crate) use instance_impl::Instance;
