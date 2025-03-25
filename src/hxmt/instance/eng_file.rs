ca8f1a45ee23b8ed38e6d6d0249ad8c6e64856e5
Haoxuan Guo <haoxuan_guo@foxmail.com>
2025-03-25 18:46:16 +0000

    修正EngFile中读取的Bus_Time_BDC列名，确保与数据文件一致

--- src/hxmt/instance/eng_file.rs
+++ src/hxmt/instance/eng_file.rs
@@ -14,7 +14,7 @@ impl EngFile {
         // HDU 1: HE_Eng
         let eng = fptr.hdu("HE_Eng")?;
         let time = eng.read_col::<i32>(&mut fptr, "Time")?;
-        let bus_time_bdc_raw: Vec<u8> = eng.read_col(&mut fptr, "Bus_Time_BDC")?;
+        let bus_time_bdc_raw: Vec<u8> = eng.read_col(&mut fptr, "BUS_Time_Bdc")?;
         let mut bus_time_bdc = Vec::with_capacity(bus_time_bdc_raw.len() / 6);
         for chunk in bus_time_bdc_raw.chunks_exact(6) {
             let mut array = [0; 6];
