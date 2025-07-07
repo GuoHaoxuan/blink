a6fce893feeacd8b52378014987ab65e88530aa4
GUO Hao-xuan <guohx@ihep.ac.cn>
2025-07-07 12:31:26 +0800

    修改 interpolate 方法以返回 Attitude，添加线性扩展逻辑以处理超出时间范围的情况

--- src/satellites/hxmt/data/data_1k/att.rs
+++ src/satellites/hxmt/data/data_1k/att.rs
@@ -47,14 +47,32 @@ impl AttFile {
         Ok(Self { time, q1, q2, q3 })
     }
 
-    pub fn interpolate(&self, time: Time<Hxmt>) -> Option<Attitude> {
+    pub fn interpolate(&self, time: Time<Hxmt>) -> Attitude {
         let time_f64 = time.time.into_inner();
         let mut i = 0;
         while i < self.time.len() - 1 && self.time[i + 1] < time_f64 {
             i += 1;
         }
         if i == self.time.len() - 1 {
-            return None;
+            // linear extension
+            let last_time = self.time[self.time.len() - 1];
+            let last_q1 = self.q1[self.q1.len() - 1];
+            let last_q2 = self.q2[self.q2.len() - 1];
+            let last_q3 = self.q3[self.q3.len() - 1];
+            let last_q1_2 = self.q1[self.q1.len() - 2];
+            let last_q2_2 = self.q2[self.q2.len() - 2];
+            let last_q3_2 = self.q3[self.q3.len() - 2];
+            let t_diff = time_f64 - last_time;
+            let ratio = t_diff / (last_time - self.time[self.time.len() - 2]);
+            let q1_interp = last_q1 + (last_q1 - last_q1_2) * ratio;
+            let q2_interp = last_q2 + (last_q2 - last_q2_2) * ratio;
+            let q3_interp = last_q3 + (last_q3 - last_q3_2) * ratio;
+            return Attitude {
+                time: time.to_chrono(),
+                q1: q1_interp,
+                q2: q2_interp,
+                q3: q3_interp,
+            };
         }
 
         let t0 = self.time[i];
@@ -71,11 +89,11 @@ impl AttFile {
         let q2_interp = q20 + (q21 - q20) * (time_f64 - t0) / (t1 - t0);
         let q3_interp = q30 + (q31 - q30) * (time_f64 - t0) / (t1 - t0);
 
-        Some(Attitude {
+        Attitude {
             time: time.to_chrono(),
             q1: q1_interp,
             q2: q2_interp,
             q3: q3_interp,
-        })
+        }
     }
 }
