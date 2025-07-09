f3c96fe474f6ac2ee7336857e2770e3d635c47b4
GUO Hao-xuan <guohx@ihep.ac.cn>
2025-07-09 17:12:35 +0800

    小测试

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -6,6 +6,8 @@ fn main() {
     let start_time_snapshot = DateTime::parse_from_rfc3339("2025-06-01T00:00:00+00:00")
         .unwrap()
         .to_utc();
+    let mut total_number: u64 = 0;
+    let mut saturation_number: u64 = 0;
     for i in 0..23 {
         let mut start_time = start_time_snapshot + TimeDelta::hours(i);
         let ins = Instance::from_epoch(&start_time).unwrap();
@@ -16,6 +18,9 @@ fn main() {
         while start_time <= stop_time {
             let time = start_time;
             let saturation = ins.check_saturation(time.into());
+            if saturation {
+                saturation_number += 1;
+            }
             if saturation && !sa {
                 println!("Saturation at: {}", time);
                 sa = true;
@@ -24,6 +29,17 @@ fn main() {
                 sa = false;
             }
             start_time += time_delta;
+            total_number += 1;
         }
     }
+    println!("Total number of checks: {}", total_number);
+    println!("Total number of saturations: {}", saturation_number);
+    println!(
+        "Saturation percentage: {:.2}%",
+        if total_number > 0 {
+            saturation_number as f64 / total_number as f64 * 100.0
+        } else {
+            0.0
+        }
+    );
 }
