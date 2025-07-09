fb9f96225edba6c5ef6bba9a25538aaa57b2c120
GUO Hao-xuan <guohx@ihep.ac.cn>
2025-07-09 16:23:38 +0800

    做个小测试

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -3,18 +3,28 @@ use blink::types::Instance as _;
 use chrono::{TimeDelta, prelude::*};
 
 fn main() {
-    let mut start_time = DateTime::parse_from_rfc3339("2025-07-06T16:45:22.626+00:00")
+    let start_time_snapshot = DateTime::parse_from_rfc3339("2025-06-01T00:00:00+00:00")
         .unwrap()
-        .to_utc()
-        - TimeDelta::seconds(10);
-    let ins = Instance::from_epoch(&start_time).unwrap();
-    let time_delta = TimeDelta::milliseconds(100);
-    let stop_time = start_time + TimeDelta::seconds(50);
-    // enum time
-    while start_time <= stop_time {
-        let time = start_time;
-        let saturation = ins.check_saturation_group(time.into());
-        println!("{:?}", saturation);
-        start_time += time_delta;
+        .to_utc();
+    for i in 0..23 {
+        let mut start_time = start_time_snapshot + TimeDelta::hours(i);
+        let ins = Instance::from_epoch(&start_time).unwrap();
+        let time_delta = TimeDelta::milliseconds(100);
+        let stop_time = start_time + TimeDelta::hours(1);
+        // enum time
+        let mut sa = false;
+        while start_time <= stop_time {
+            let time = start_time;
+            let saturation = ins.check_saturation(time.into());
+            if saturation && !sa {
+                println!("Saturation at: {}", time);
+                sa = true;
+            }
+            if !saturation && sa {
+                sa = false;
+            }
+            println!("{:?}", saturation);
+            start_time += time_delta;
+        }
     }
 }
