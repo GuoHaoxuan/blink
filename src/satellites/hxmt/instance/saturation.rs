a574386e6696c451a742f148cea9b707fbd69e7e
GUO Hao-xuan <guohx@ihep.ac.cn>
2025-07-11 14:26:14 +0800

    尝试一下在饱和处加一个符合

--- src/satellites/hxmt/instance/saturation.rs
+++ src/satellites/hxmt/instance/saturation.rs
@@ -7,7 +7,7 @@ use crate::types::Time;
 impl Instance {
     pub fn check_saturation(&self, time: Time<Hxmt>) -> bool {
         rec_sci_data(time, &self.eng_files[0], &self.sci_files[0])
-            || rec_sci_data(time, &self.eng_files[1], &self.sci_files[1])
-            || rec_sci_data(time, &self.eng_files[2], &self.sci_files[2])
+            && rec_sci_data(time, &self.eng_files[1], &self.sci_files[1])
+            && rec_sci_data(time, &self.eng_files[2], &self.sci_files[2])
     }
 }
