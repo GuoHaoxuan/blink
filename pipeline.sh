e3cac902c4defa14c020519677e98427f8988301
GUO Hao-xuan <guo.haoxuan@outlook.com>
2025-08-29 12:55:36 +0800

    优化脚本结构，调整注释，清理日志文件，更新数据库准备步骤

--- pipeline.sh
+++ pipeline.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-# 0. 检查上一次任务是不是还没计算完
+export PATH=/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin:$PATH
+cd /scratchfs/gecam/guohx/pipeline
+
+# 检查上一次任务是不是还没计算完
 declare -i current_jobs=$(hep_q -u | wc -l)
 if [ $current_jobs -ne 4 ]; then
   echo "Previous jobs are still running. Exiting."
   exit 0
 fi
 
-# 1. 拉取最新的代码
-git pull
+# 清理上次的日志
+comm -23 \
+  <(find . \( -name "astro_sift.py.out.*" -or -name "astro_sift.py.err.*" \) -and -size 0 | sort) \
+  <(hep_q -u | grep astro_sift.py | awk '{ print "./astro_sift.py.out." $1 "\n./astro_sift.py.err." $1 }' | sort) \
+  | xargs rm -rf
 
-# 2. 编译代码
-cargo build --release
+# 拉取最新的代码
+# git pull
 
-# 3. 准备数据库
-sqlite3 blink.db < sql/init.sql
+# 编译代码
+# cargo build --release
+
+# 准备数据库
+# sqlite3 blink.db < sql/init.sql
 sqlite3 blink.db < sql/refresh_task.sql
 
-# 4. 准备软链接
-ln -sf ../target/release/snapshot-stepping ./snapshot-stepping
+# 准备软链接
+# ln -sf ./target/release/work ./work
+# ln -sf ./target/release/render ./render
+
+# 提交运行
+hep_sub -mem 8192 -g hxmt work
