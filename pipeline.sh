2e34296ddb8b5e641d7e53d1f8836691fb0bf27e
GUO Hao-xuan <guo.haoxuan@outlook.com>
2025-09-02 15:04:23 +0800

    更新pipeline的脚本

--- pipeline.sh
+++ pipeline.sh
@@ -4,31 +4,27 @@ export PATH=/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin:$PATH
 cd /scratchfs/gecam/guohx/pipeline
 
 # 检查上一次任务是不是还没计算完
-declare -i current_jobs=$(hep_q -u | wc -l)
-if [ $current_jobs -ne 4 ]; then
+if [ $(hep_q -u | wc -l) -ne 4 ]; then
   echo "Previous jobs are still running. Exiting."
   exit 0
 fi
 
-# 清理上次的日志
-comm -23 \
-  <(find . \( -name "astro_sift.py.out.*" -or -name "astro_sift.py.err.*" \) -and -size 0 | sort) \
-  <(hep_q -u | grep astro_sift.py | awk '{ print "./astro_sift.py.out." $1 "\n./astro_sift.py.err." $1 }' | sort) \
-  | xargs rm -rf
+if [ $(ls | grep "work." | wc -l) -ne 0 ]; then
+  echo "Previous Task: Calculation."
+  comm -23 \
+    <(find . \( -name "work.out.*" -or -name "work.err.*" \) -and -size 0 | sort) \
+    <(hep_q -u | grep work.py | awk '{ print "./work.py.out." $1 "\n./work.py.err." $1 }' | sort) \
+    | xargs rm -rf
 
-# 拉取最新的代码
-# git pull
+  hep_sub -mem 8192 -g hxmt render
 
-# 编译代码
-# cargo build --release
+else
+  echo "Previous Task: Rendering."
+  comm -23 \
+    <(find . \( -name "render.out.*" -or -name "render.err.*" \) -and -size 0 | sort) \
+    <(hep_q -u | grep render.py | awk '{ print "./render.py.out." $1 "\n./render.py.err." $1 }' | sort) \
+    | xargs rm -rf
 
-# 准备数据库
-# sqlite3 blink.db < sql/init.sql
-sqlite3 blink.db < sql/refresh_task.sql
-
-# 准备软链接
-# ln -sf ./target/release/work ./work
-# ln -sf ./target/release/render ./render
-
-# 提交运行
-hep_sub -mem 8192 -g hxmt work
+  sqlite3 blink.db < sql/refresh_task.sql
+  hep_sub -mem 8192 -g hxmt work
+fi
