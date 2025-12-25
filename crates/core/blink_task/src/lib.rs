47b52531d95fa8a36c77c05e0876e4d48d7bbb3a
GUO Hao-xuan <guo.haoxuan@outlook.com>
2025-12-25 20:58:47 +0800

    在单日内更好的进度条

--- crates/core/blink_task/src/lib.rs
+++ crates/core/blink_task/src/lib.rs
@@ -6,7 +6,12 @@ pub fn process_day<C: Chunk>(day: NaiveDate) {
     let mut all_signals = Vec::new();
     let mut errors: Vec<blink_core::error::Error> = Vec::new();
 
-    for hour in (0..24).progress() {
+    for hour in (0..24).progress_with_style(
+        indicatif::ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta}) {msg}")
+        .unwrap()
+        .progress_chars("#>-")
+    ) {
         let naive = day.and_hms_opt(hour, 0, 0).expect("invalid time");
         match C::from_epoch(&Utc.from_utc_datetime(&naive)) {
             Ok(chunk) => {
