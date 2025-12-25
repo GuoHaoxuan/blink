de4dc8b350981723c1797d18fec9eb255202265e
GUO Hao-xuan <guo.haoxuan@outlook.com>
2025-12-25 20:38:31 +0800

    尝试增加一点输出来调试

--- crates/core/blink/src/main.rs
+++ crates/core/blink/src/main.rs
@@ -5,6 +5,12 @@ fn main() {
     let hxmt_he_launch_day = NaiveDate::from_ymd_opt(2017, 6, 22).unwrap();
     let today = Utc::now().naive_utc().date();
     let days_since_launch = (today - hxmt_he_launch_day).num_days();
+    println!(
+        "Processing HXMT-HE data from launch day ({}) to today ({}), total {} days.",
+        hxmt_he_launch_day,
+        today,
+        days_since_launch + 1
+    );
     let progress_bar = ProgressBar::new(days_since_launch as u64 + 1);
     for day_offset in 0..=days_since_launch {
         let day = hxmt_he_launch_day + chrono::Duration::days(day_offset);
