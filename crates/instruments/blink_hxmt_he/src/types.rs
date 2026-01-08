5c95ed13364fe00995eea38352b93bdf8bd837ce
Guo Hao-Xuan <guo.haoxuan@outlook.com>
2026-01-09 00:26:47 +0800

    重构代码，将 satellite 模块重命名为 instrument，并更新相关引用以支持新的模块结构

--- crates/instruments/blink_hxmt_he/src/types.rs
+++ crates/instruments/blink_hxmt_he/src/types.rs
@@ -1,9 +1,9 @@
 pub mod chunk;
 pub mod detector;
 pub mod event;
-pub mod satellite;
+pub mod instrument;
 
 pub use chunk::Chunk;
 pub use detector::{Detector, Scintillator};
 pub use event::Event;
-pub use satellite::HxmtHe;
+pub use instrument::HxmtHe;
