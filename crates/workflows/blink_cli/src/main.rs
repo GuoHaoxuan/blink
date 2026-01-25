bf1d616c9cfbb4b35741ac335fa9d1fa8626d5e9
Guo Hao-Xuan <guo.haoxuan@outlook.com>
2026-01-26 03:07:32 +0800

    尝试用命令行做饱和判断

--- crates/workflows/blink_cli/src/main.rs
+++ crates/workflows/blink_cli/src/main.rs
@@ -1,5 +1,41 @@
-use blink_filter::run;
+// use blink_filter::run;
+use blink_core::{traits::Chunk as _, types::MissionElapsedTime};
+use blink_hxmt_he::types::Chunk;
+use chrono::prelude::*;
+use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    check_hxmt_saturation: String,
+}
 fn main() {
-    run();
+    let args = Args::parse();
+
+    if !args.check_hxmt_saturation.is_empty() {
+        let time = (args.check_hxmt_saturation + "Z")
+            .parse::<DateTime<Utc>>()
+            .unwrap();
+        let chunk = Chunk::from_epoch(&time);
+        if chunk.is_err() {
+            println!("No data chunk found for the specified time: {}", time);
+            return;
+        }
+        let chunk = chunk.unwrap();
+        let met: MissionElapsedTime<blink_hxmt_he::types::HxmtHe> = time.into();
+        let saturated = chunk.check_saturation(met);
+
+        if saturated {
+            println!(
+                "The HXMT/HE data is saturated at the specified time: {}",
+                time
+            );
+        } else {
+            println!(
+                "The HXMT/HE data is NOT saturated at the specified time: {}",
+                time
+            );
+        }
+    }
 }
