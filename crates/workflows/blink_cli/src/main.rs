6d3e9f345a765d5c05a492ba706057debfb8902e
Guo Hao-Xuan <guo.haoxuan@outlook.com>
2026-01-26 15:51:54 +0800

    尝试做一个饱和判断

--- crates/workflows/blink_cli/src/main.rs
+++ crates/workflows/blink_cli/src/main.rs
@@ -1,41 +1,20 @@
 // use blink_filter::run;
 use blink_core::{traits::Chunk as _, types::MissionElapsedTime};
-use blink_hxmt_he::types::Chunk;
+use blink_hxmt_he::types::{Chunk, HxmtHe};
 use chrono::prelude::*;
 use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    check_hxmt_saturation: String,
-}
 fn main() {
-    let args = Args::parse();
-
-    if !args.check_hxmt_saturation.is_empty() {
-        let time = (args.check_hxmt_saturation + "Z")
-            .parse::<DateTime<Utc>>()
-            .unwrap();
-        let chunk = Chunk::from_epoch(&time);
-        if chunk.is_err() {
-            println!("No data chunk found for the specified time: {}", time);
-            return;
-        }
-        let chunk = chunk.unwrap();
-        let met: MissionElapsedTime<blink_hxmt_he::types::HxmtHe> = time.into();
+    let start = "2022-10-09T13:20:00.000Z".parse::<DateTime<Utc>>().unwrap();
+    let stop = "2022-10-09T13:22:00.000Z".parse::<DateTime<Utc>>().unwrap();
+    let chunk = Chunk::from_epoch(&start).unwrap();
+    // check for every 0.1s
+    let mut cursor = start;
+    while cursor < stop {
+        let met: MissionElapsedTime<HxmtHe> = cursor.into();
         let saturated = chunk.check_saturation(met);
-
-        if saturated {
-            println!(
-                "The HXMT/HE data is saturated at the specified time: {}",
-                time
-            );
-        } else {
-            println!(
-                "The HXMT/HE data is NOT saturated at the specified time: {}",
-                time
-            );
-        }
+        println!("{saturated}");
+        let next = cursor + chrono::Duration::milliseconds(100);
+        cursor = next;
     }
 }
